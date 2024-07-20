8a62ffd18a963cd7d8ed0cc0c6aa50ade0215c2c
Kai'Ru <97433017+KaiRu-0@users.noreply.github.com>
2024-07-21 00:32:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 # Data Collection and Pre-Processing
 
 # loading the data from the csv file to a pandas dataframe
-movies_data = pd.read_csv('movies_data.csv')
+movies_data = pd.read_csv('movies.csv')
 
 # printing the first 5 rows of the dataframe
 print(movies_data.head())
@@ -71,33 +71,6 @@ sorted_similar_movies = sorted(similarity_score, key=lambda x: x[1], reverse=Tru
 print(sorted_similar_movies)
 
 # print the name of similar movies based on the index
-print('Movies suggested for you: \n')
-
-i = 1
-for movie in sorted_similar_movies:
-    index = movie[0]
-    title_from_index = movies_data[movies_data.index == index]['title'].values[0]
-    if i < 30:
-        print(i, '.', title_from_index)
-        i += 1
-
-# Movie Recommendation System
-
-# repeating the process for user input
-movie_name = input('Enter your favourite movie name: ')
-
-list_of_all_titles = movies_data['title'].tolist()
-
-find_close_match = difflib.get_close_matches(movie_name, list_of_all_titles)
-
-close_match = find_close_match[0]
-
-index_of_the_movie = movies_data[movies_data.title == close_match]['index'].values[0]
-
-similarity_score = list(enumerate(similarity[index_of_the_movie]))
-
-sorted_similar_movies = sorted(similarity_score, key=lambda x: x[1], reverse=True)
-
 print('Movies suggested for you: \n')
 
 i = 1
