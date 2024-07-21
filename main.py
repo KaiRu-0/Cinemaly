e0efb9e194e2f85d75f5d27d66d9c5cbf06b2c1e
Kai'Ru <97433017+KaiRu-0@users.noreply.github.com>
2024-07-21 11:07:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,20 +5,14 @@ import difflib
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+
 # Data Collection and Pre-Processing
 
 # loading the data from the csv file to a pandas dataframe
 movies_data = pd.read_csv('movies.csv')
 
-# printing the first 5 rows of the dataframe
-print(movies_data.head())
-
-# number of rows and columns in the data frame
-print(movies_data.shape)
-
 # selecting the relevant features for recommendation
 selected_features = ['genres', 'keywords', 'tagline', 'cast', 'director']
-print(selected_features)
 
 # replacing the null values with empty strings
 for feature in selected_features:
@@ -26,53 +20,43 @@ for feature in selected_features:
 
 # combining all the selected features into a single string
 combined_features = movies_data['genres'] + ' ' + movies_data['keywords'] + ' ' + movies_data['tagline'] + ' ' + movies_data['cast'] + ' ' + movies_data['director']
-print(combined_features)
 
 # converting the text data to feature vectors
 vectorizer = TfidfVectorizer()
 feature_vectors = vectorizer.fit_transform(combined_features)
-print(feature_vectors)
+
 
 # Cosine Similarity
 
 # getting the similarity scores using cosine similarity
 similarity = cosine_similarity(feature_vectors)
-print(similarity)
-print(similarity.shape)
 
-# Getting the movie name from the user
+
+# Reccomending Movies
 
 # getting the movie name from the user
 movie_name = input('Enter your favourite movie name: ')
 
 # creating a list with all the movie names given in the dataset
 list_of_all_titles = movies_data['title'].tolist()
-print(list_of_all_titles)
 
 # finding the close match for the movie name given by the user
 find_close_match = difflib.get_close_matches(movie_name, list_of_all_titles)
-print(find_close_match)
 
 # selecting the closest match
 close_match = find_close_match[0]
-print(close_match)
 
 # finding the index of the movie with title
 index_of_the_movie = movies_data[movies_data.title == close_match]['index'].values[0]
-print(index_of_the_movie)
 
 # getting a list of similar movies
 similarity_score = list(enumerate(similarity[index_of_the_movie]))
-print(similarity_score)
-print(len(similarity_score))
 
 # sorting the movies based on their similarity score
 sorted_similar_movies = sorted(similarity_score, key=lambda x: x[1], reverse=True)
-print(sorted_similar_movies)
 
 # print the name of similar movies based on the index
 print('Movies suggested for you: \n')
-
 i = 1
 for movie in sorted_similar_movies:
     index = movie[0]
